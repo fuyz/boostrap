f82d5c2946ee804d975f8867d8bd1eb50feccfca
fuyz <953958160@qq.com>
2017-03-20 18:22:45 +0800

    my first bootstrp with grunt

---
+++ dist/boostrap.js
@@ -0,0 +1,12 @@
+/*! boostrap - v0.1.0 - 2017-03-14
+* https://github.com/zhihe/boostrap
+* Copyright (c) 2017 fuyz; Licensed MIT */
+(function(exports) {
+
+  'use strict';
+
+  exports.awesome = function() {
+    return 'awesome';
+  };
+
+}(typeof exports === 'object' && exports || this));
